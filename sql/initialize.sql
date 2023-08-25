b6d086b07ed0771ac605561d7137e2db3c8434c5
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-08-24 20:35:52 -0500

    werk

--- sql/initialize.sql
+++ sql/initialize.sql
@@ -9,15 +9,17 @@
 -- optional: create payments table (might not do this at all)
     -- insert into payments referencing orderId
 
-DROP TABLE IF EXISTS clonestation_ship_to;
-DROP TABLE IF EXISTS clonestation_ship_from;
-DROP TABLE IF EXISTS clonestation_order_items;
-DROP TABLE IF EXISTS clonestation_products;
-DROP TABLE IF EXISTS clonestation_orders;
-DROP TABLE IF EXISTS clonestation_shipments;
-DROP TABLE IF EXISTS clonestation_customers;
+DROP TABLE IF EXISTS OrderPost_ship_to;
 
-CREATE TABLE clonestation_customers (
+DROP TABLE IF EXISTS OrderPost_order_items;
+DROP TABLE IF EXISTS OrderPost_products;
+DROP TABLE IF EXISTS OrderPost_orders;
+DROP TABLE IF EXISTS OrderPost_shipments;
+DROP TABLE IF EXISTS OrderPost_customers;
+
+DROP TABLE IF EXISTS OrderPost_warehouses; -- needs to be last I think
+
+CREATE TABLE OrderPost_customers (
     id INT PRIMARY KEY auto_increment,
     first_name VARCHAR (40),
     last_name VARCHAR (40),
@@ -25,7 +27,7 @@ CREATE TABLE clonestation_customers (
     email VARCHAR (50)
 );
 
-CREATE TABLE clonestation_ship_to (
+CREATE TABLE OrderPost_ship_to (
     id INT PRIMARY KEY auto_increment,
     customer_id INT,
     first_name VARCHAR (40),
@@ -42,11 +44,11 @@ CREATE TABLE clonestation_ship_to (
     country_code VARCHAR (10),
     address_residential_indicator BOOLEAN,
     FOREIGN KEY (customer_id) 
-    REFERENCES clonestation_customers (id)
+    REFERENCES OrderPost_customers (id)
     ON DELETE CASCADE
 );
 
-CREATE TABLE clonestation_ship_from (
+CREATE TABLE OrderPost_warehouses (
     id INT PRIMARY KEY auto_increment,
     first_name VARCHAR (40),
     last_name VARCHAR (40),
@@ -64,7 +66,7 @@ CREATE TABLE clonestation_ship_from (
     address_residential_indicator BOOLEAN
 );
 
-CREATE TABLE clonestation_orders (
+CREATE TABLE OrderPost_orders (
     order_id INT PRIMARY KEY auto_increment,
     customer_id INT,
     order_number VARCHAR (20) NOT NULL,
@@ -84,42 +86,42 @@ CREATE TABLE clonestation_orders (
     dimension_y FLOAT (5,2),
     dimension_z FLOAT (5,2),
     dimension_units VARCHAR (5),
-    ship_from_id INT,
-    FOREIGN KEY (ship_from_id)
-    REFERENCES clonestation_ship_from (id)
+    warehouses_id INT,
+    FOREIGN KEY (warehouses_id)
+    REFERENCES OrderPost_warehouses (id)
     ON DELETE CASCADE,
         -- (M,D) means than values can be stored with up to M digits in total, of which D digits may be after the decimal point
      FOREIGN KEY (customer_id) 
-     REFERENCES clonestation_customers (id)
+     REFERENCES OrderPost_customers (id)
      ON DELETE CASCADE
 );
 
 -- re-testing comment
 
-CREATE TABLE clonestation_products (
+CREATE TABLE OrderPost_products (
     product_id INT PRIMARY KEY auto_increment,
     product_name VARCHAR (30),
     price DECIMAL (10,2),
     description VARCHAR (150)
 );
 
-CREATE TABLE clonestation_order_items (
+CREATE TABLE OrderPost_order_items (
     order_item_id INT PRIMARY KEY auto_increment,
     order_id INT,
     product_id INT,
     quantity INT,
 
     FOREIGN KEY (order_id)
-    REFERENCES clonestation_orders (order_id)
+    REFERENCES OrderPost_orders (order_id)
     ON DELETE CASCADE,
     FOREIGN KEY (product_id)
     REFERENCES 
-    clonestation_products (product_id)
+    OrderPost_products (product_id)
     ON DELETE CASCADE
 );
 
 -- once a shipping label is created, we can insert into Shipments table
-CREATE TABLE clonestation_shipments (
+CREATE TABLE OrderPost_shipments (
     shipment_id INT PRIMARY KEY auto_increment,
     -- Shipments are generated from orders, not customers
     -- customer_id INT,
@@ -142,7 +144,7 @@ CREATE TABLE clonestation_shipments (
     dimension_units VARCHAR (5),
         -- (M,D) means than values can be stored with up to M digits in total, of which D digits may be after the decimal point
      FOREIGN KEY (order_id)
-     REFERENCES clonestation_orders (order_id)
+     REFERENCES OrderPost_orders (order_id)
      ON DELETE CASCADE
 );
 
