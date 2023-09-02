787cccac5847e2f7b5cd006e9988e60aab65172c
austinc512 <cello.austin1994@gmail.com>
2023-09-01 20:56:00 -0500

    API validation is a unique layer of hell.

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -4,59 +4,108 @@ const db = require("../sql/db");
 
 const listCustomers = (req, res) => {
   const userId = req.userInfo.userId;
-  let sql = `SELECT * FROM OrderPost_customers WHERE user_id = ${userId} LIMIT 100`;
-  // I still need to implement sizing
+  let size = 100; // default value
+  let offset = 0; // default value
+  let errors = [];
+
+  /*
+  later, offset can be multiplied by page number for pagination.
+  */
+
   if (req.query.size) {
-    console.log(`size is being sent`);
-    const size = +req.query.size;
-    if (Number.isFinite(size)) {
-      console.log(`size is a number`);
-      if (size > 500) {
-        res.status(400).json(`size param is too big`);
-        return;
-      } else if (size < 1) {
-        res.status(400).json(`size param is too small`);
-        return;
-      }
-      sql = `SELECT * FROM OrderPost_customers WHERE user_id = ${userId} LIMIT ${size}`;
-    } else {
-      console.log(`size is sent, but size cannot be coerced into a number`);
-      res.status(400).json(`size param looks incorrect`);
-      return;
+    size = +req.query.size;
+    if (!Number.isFinite(size) || size > 500 || size < 1) {
+      errors.push({
+        status: "error",
+        message:
+          "Invalid 'size' parameter. It must be a number between 1 and 500.",
+        code: 400,
+      });
     }
   }
-  db.query(sql, (err, rows) => {
-    if (err) {
-      console.log(`${sql} failed:`);
-      console.log(err);
-      res.status(500).json(err);
-      return;
-    } else {
-      console.log(`${sql} returned results:`);
-      console.log(rows);
-      res.json(rows);
-      return;
+
+  if (req.query.offset) {
+    offset = +req.query.offset;
+    if (!Number.isFinite(offset) || offset < 0) {
+      errors.push({
+        status: "error",
+        message: "Invalid 'offset' parameter. It must be a positive number.",
+        code: 400,
+      });
     }
+  }
+
+  if (errors.length) {
+    return res.status(400).json({ errors });
+  }
+  const sql =
+    "SELECT * FROM OrderPost_customers WHERE user_id = ? LIMIT ? OFFSET ?";
+  db.query(sql, [userId, size, offset], (err, rows) => {
+    if (err) {
+      console.error("SQL query failed:", err);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+        code: 500,
+      });
+    }
+
+    res.json({
+      data: rows,
+      meta: {
+        size,
+        offset,
+      },
+    });
   });
-  // I still need to implement sizing
 };
 
 const getCustomerById = (req, res) => {
-  const customerId = [req.params.customerId];
-  let sql = "SELECT * FROM OrderPost_customers WHERE customer_id = ?";
-  db.query(sql, customerId, (err, rows) => {
+  let errors = [];
+  const customerId = +req.params.customerId;
+  const userId = req.userInfo.userId;
+
+  if (!Number.isFinite(customerId)) {
+    errors.push({
+      status: "error",
+      message: "Invalid 'customerId' parameter. It must be an integer.",
+      code: 400,
+    });
+  }
+
+  if (errors.length) {
+    return res.status(400).json({ errors });
+  }
+
+  let sql =
+    "SELECT * FROM OrderPost_customers WHERE customer_id = ? AND user_id = ?";
+  const params = [customerId, userId];
+  db.query(sql, params, (err, rows) => {
     if (err) {
       console.log(`SELECT from OrderPost_customers by ID failed:`);
       console.log(err);
+      return res.status(500).json({
+        errors: {
+          status: "error",
+          message: "Internal Server Error",
+          code: 500,
+        },
+      });
     } else {
       if (rows.length === 0) {
         console.log(`SELECT from OrderPost_customers by ID returned 0 results`);
-        res.status(400).json(`There are no customers with this ID`);
+        res.status(404).json({
+          errors: {
+            status: "error",
+            message: "No customer with this ID was found",
+            code: 404,
+          },
+        });
         return;
       } else {
         console.log("SELECT from OrderPost_customers by ID returned results: ");
         console.log(rows);
-        res.json(rows[0]);
+        res.json({ data: rows[0] });
       }
     }
   });
@@ -72,13 +121,18 @@ const createCustomer = (req, res) => {
 }
   */
 
+  const errors = [];
+  const userId = req.userInfo.userId;
+
   const { first_name, last_name, phone, email } = req.body;
   if (!req.body || !first_name || !last_name || !phone || !email) {
     console.log("A falsy value was sent in createCustomer request body");
-    res
-      .status(400)
-      .json("A required property in the request has a falsy value");
-    return;
+    errors.push({
+      status: "error",
+      message:
+        "A falsy value was sent in createCustomer request body (check for missing required properties)",
+      code: 400,
+    });
   }
   if (
     typeof first_name != "string" ||
@@ -89,14 +143,23 @@ const createCustomer = (req, res) => {
     console.log(
       "1 or more properties in the createCustomer request body are not of type string"
     );
-    res.status(400).json("1 or more properties are not of type string");
+    errors.push({
+      status: "error",
+      message: "1 or more properties are not of type string",
+      code: 400,
+    });
+  }
+
+  if (errors.length) {
+    res.status(400).json({ errors });
     return;
   }
+
   // if we get here, we have all properties, and they are strings.
   let sql =
-    "INSERT into OrderPost_customers (first_name, last_name, phone, email) VALUES (?, ?, ?, ?)";
+    "INSERT into OrderPost_customers (first_name, last_name, phone, email, user_id) VALUES (?, ?, ?, ?, ?)";
 
-  let params = [first_name, last_name, phone, email];
+  let params = [first_name, last_name, phone, email, userId];
 
   db.query(sql, params, (err, dbResponse) => {
     if (err) {
@@ -106,23 +169,49 @@ const createCustomer = (req, res) => {
       // I'm not enforcing uniqueness on anything in this table right now (other than auto ID)
       // that could change later, so I'm still using this pattern
       if (err.code == "ER_DUP_ENTRY") {
-        res.status(400).json(`not a unique customer`);
-        return;
+        return res.status(400).json({
+          errors: {
+            status: "error",
+            message: "Not a unique customer",
+            code: 500,
+          },
+        });
       } else {
-        res.status(500).json(`something failed`);
-        return;
+        return res.status(500).json({
+          errors: {
+            status: "error",
+            message: "Internal Server Error",
+            code: 500,
+          },
+        });
       }
     } else {
       console.log(`insert into OrderPost_customers succeeded:`);
       console.log(dbResponse);
-      return res.json(dbResponse);
+      return res.json({ data: { dbResponse } });
     }
   });
 };
 
 const updateCustomer = (req, res) => {
-  const customerId = [req.params.customerId];
+  const customerId = +req.params.customerId;
+  const userId = req.userInfo.userId;
   console.log(customerId);
+
+  let errors = [];
+
+  if (!Number.isFinite(customerId)) {
+    errors.push({
+      status: "error",
+      message: "Invalid 'customerId' parameter. It must be an integer.",
+      code: 400,
+    });
+  }
+
+  if (errors.length) {
+    return res.status(400).json({ errors });
+  }
+
   let requestPropertyCounter = 0;
   const keys = Object.keys(req.body);
   console.log(keys);
@@ -138,17 +227,75 @@ const updateCustomer = (req, res) => {
   }
   if (requestPropertyCounter == 0) {
     console.log("updateCustomer request body contained no valid properties");
-    res
-      .status(400)
-      .json(`request body contained no valid properties to update`);
-    return;
+    return res.status(400).json({
+      errors: {
+        status: "error",
+        message: "request body contained no valid properties to update",
+        code: 400,
+      },
+    });
   }
   // if here, then request body contains something to update.
-  // is there some iterative way I can update the sql statement from the properties?
   const { first_name, last_name, phone, email } = req.body;
-  console.log(first_name);
-  res.json("still testing brb");
+  let sql = "UPDATE OrderPost_customers SET ";
+  const params = [];
+
+  if (first_name && typeof first_name == "string") {
+    sql += `first_name = ?, `;
+    params.push(first_name);
+  }
+  if (last_name && typeof last_name == "string") {
+    sql += `last_name = ?, `;
+    params.push(last_name);
+  }
+  if (phone && typeof phone == "string") {
+    sql += `phone = ?, `;
+    params.push(phone);
+  }
+  if (email && typeof email == "string") {
+    sql += `email = ? `;
+    params.push(email);
+  }
+
+  sql += "WHERE customer_id = ? AND user_id = ?";
+  params.push(customerId, userId);
+  console.log(sql);
+  console.log(params);
+  db.query(sql, params, (err, dbResponse) => {
+    // do something
+    console.log(dbResponse);
+    console.log(dbResponse.affectedRows);
+    console.log(dbResponse.affectedRows === 0);
+    if (dbResponse.affectedRows === 0) {
+      res.status(400).json({
+        errors: {
+          status: "error",
+          message: "invalid customer_id",
+          code: 400,
+        },
+      });
+    } else if (err) {
+      {
+        res.status(500).json({
+          errors: {
+            status: "error",
+            message: "internal server error",
+            code: 500,
+          },
+        });
+      }
+    } else {
+      res.json({ data: dbResponse });
+    }
+  });
 };
+
+/*
+
+If nothing gets updated:
+0 row(s) affected Rows matched: 0  Changed: 0  Warnings: 0
+
+*/
 
 const deleteCustomer = (req, res) => {
   // creating scaffolding
